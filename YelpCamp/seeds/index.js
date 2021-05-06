f14f64fac152c8c167a40beaead94bb2bd6fd913
cade-kreikemeier <43ee40c50b391d6eed9e61c78da2b60d>
2021-05-06 17:24:07 -0600

    Added basic styling to YelpCamp

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
+			image: 'https://source.unsplash.com/collection/483251',
+			description:
+				'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia et laborum dignissimos sequi facilis, tempore, vel consequatur minima nesciunt quo incidunt numquam aspernatur perspiciatis iusto temporibus ipsam, officiis ratione labore. Doloribus, dolorem nam quo aut quia nihil rerum maxime a, voluptatum consequuntur odit assumenda, praesentium animi sunt cumque ipsam eligendi?',
+			price: price,
 		});
 		await camp.save();
 	}
